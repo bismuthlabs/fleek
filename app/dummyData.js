47bd9c684201b863d06a7b7ba6ec5ef9fcc52a97
Divine Quansah <divquan@gmail.com>
2023-06-20 04:57:04 +0000

    added dummyData

---
+++ app/dummyData.js
@@ -0,0 +1,101 @@
+import heroImage from "./assets/images/herosection.jpg";
+import heroImage1 from "./assets/images/herosection2.jpg";
+
+export const herosection = [
+  {
+    id: 0,
+    heading: "Women",
+    tag: "Shop all women products",
+    img: heroImage1,
+    btnName: "Shop now",
+    btnLink: "",
+  },
+  {
+    id: 1,
+    heading: "Men",
+    tag: "Shop all men products",
+    img: heroImage,
+    btnName: "Shop now",
+    btnLink: "",
+  },
+];
+
+export const catMen = [
+  {
+    name: "Chuck Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Chuck Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Chuck Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Chuck Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Chuck Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Chuck Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+];
+
+export const catWomen = [
+  {
+    name: "Chuck Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Women Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Women Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Women Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Women Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+  {
+    name: "Women Taylor All Star Leather Platform",
+    price: 399,
+    discount: 699,
+    image: "",
+  },
+];
+
+export const accordicon = [{}];
